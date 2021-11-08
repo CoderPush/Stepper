c6b1c19c0c4ba9f768e3fcc9e324fe16c5e4ecf2
Minh Kim Long <36654067+KimLong1994@users.noreply.github.com>
2021-11-08 18:09:47 +0700

    re-write area test (#61)
    Co-authored-by: Minh Kim Long <minhkimlong94@gmail.com>

--- test_integration/area_test.dart
+++ test_integration/area_test.dart
@@ -7,6 +7,8 @@ import 'package:stepper/main.dart' as app;
 import 'package:stepper/common/texts.dart';
 import 'package:stepper/common/consts.dart';
 
+import 'package:stepper/presentation/create_post/views/area_dropdown.dart';
+
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
@@ -16,6 +18,40 @@ void main() {
       testWidgets("Check existing widgets and their behavior",
           (WidgetTester tester) async {
         app.main();
+        await tester.pumpAndSettle();
+
+        // Test floating action button, navigate to create post screen
+        expect(find.byIcon(Icons.add), findsOneWidget);
+        final Finder finderCreatePostButton = find.byTooltip(createPostButton);
+        await tester.tap(finderCreatePostButton);
+        await tester.pumpAndSettle();
+
+        // Test open drop down
+        final Finder finderAreaDropDown = find.byType(AreaDropdown);
+        await tester.tap(finderAreaDropDown);
+        await tester.pumpAndSettle();
+
+        // Test close drop down
+        final Finder finderCreatePostScreen = find.byType(MaterialApp);
+        await tester.tap(finderCreatePostScreen);
+        await tester.pumpAndSettle();
+
+        // Test write update
+        await tester.tap(find.text(writeUpdate));
+        await tester.pumpAndSettle();
+
+        final Finder finderUpdateTextField = find.byType(TextFormField);
+        await tester.enterText(finderUpdateTextField, writeUpdateText);
+        await tester.pumpAndSettle();
+
+        // Test post button
+        await tester.tap(find.text(setGoal));
+        await tester.pumpAndSettle();
+        await tester.tap(find.text(writeUpdate));
+        await tester.pumpAndSettle();
+
+        final Finder finderPostButton = find.byKey(const Key(postButton));
+        await tester.tap(finderPostButton);
         await tester.pumpAndSettle();
 
         // Test drawer, open drawer
@@ -28,15 +64,24 @@ void main() {
         await tester.tap(find.text(area));
         await tester.pumpAndSettle();
 
-        // Test tab bar, switch title and color
-        await tester.tap(find.text(mindset));
+        // Test check looking for a new post
+        await tester.tap(find.text(defaultAreaName));
         await tester.pumpAndSettle();
 
-        await tester.tap(find.text(expertise));
+        final Finder finderPostListScrollView = find.byType(SingleChildScrollView);
+        await tester.drag(finderPostListScrollView, const Offset(0.0, -5000));
         await tester.pumpAndSettle();
 
-        await tester.tap(find.text(scope));
+        expect(
+            find.byWidgetPredicate((widget) =>
+                widget is RichText && widget.text.toPlainText() == writeUpdateText),
+            findsOneWidget);
+
+        await tester.tap(find.byWidgetPredicate((widget) =>
+            widget is RichText && widget.text.toPlainText() == writeUpdateText));
         await tester.pumpAndSettle();
+
+        expect(find.text(writeUpdateText), findsOneWidget);
       });
     },
   );
