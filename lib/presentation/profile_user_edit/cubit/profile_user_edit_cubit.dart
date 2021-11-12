d5d609c0e4330bcf33841287e448ca9eb14e31eb
Minh Kim Long <minhkimlong94@gmail.com>
2021-11-12 21:27:53 +0700

    remove unused import

--- lib/presentation/profile_user_edit/cubit/profile_user_edit_cubit.dart
+++ lib/presentation/profile_user_edit/cubit/profile_user_edit_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:stepper/data/datasources/local/band_database.dart';
 import 'package:stepper/data/model/band/band_item_model.dart';
 import 'package:stepper/domain/repositories/band_repository.dart';
 import 'package:stepper/domain/repositories/profession_repository.dart';
