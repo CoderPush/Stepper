218e56736c25b27ac3759d6f504f2caa5832fe08
Hung Nguyen <hung.nguyen@signanthealth.com>
2021-11-23 20:37:22 +0200

    Remove print

--- lib/config/extensions/string_extensions.dart
+++ lib/config/extensions/string_extensions.dart
@@ -3,8 +3,6 @@ import 'package:stepper/data/model/routing_data.dart';
 extension StringExtension on String {
   RoutingData get getRoutingData {
     var uriData = Uri.parse(this);
-    print(uriData.queryParameters);
-    print(uriData.path);
     return RoutingData(
       route: uriData.path,
       queryParameter: uriData.queryParameters,
