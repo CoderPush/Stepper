5e8fecf8c6e4e0ecdd22a17a2b78dbed7abf84a7
Hung Nguyen <hung.nguyen@signanthealth.com>
2021-11-29 22:50:34 +0200

    Set up Firebase for Android/iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stepper/common/palette.dart';
@@ -10,6 +11,7 @@ import 'package:stepper/presentation/profile_user_edit/cubit/profile_user_edit_c
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
