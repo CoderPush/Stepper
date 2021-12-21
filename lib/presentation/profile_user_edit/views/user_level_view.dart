c32950468da004839cb3818157518e1c56db9d01
Hung Nguyen <33485572+Harrisonnguyen1210@users.noreply.github.com>
2021-12-21 18:20:53 +0200

    Warning shown when changing level/profession without internet (#151)

--- lib/presentation/profile_user_edit/views/user_level_view.dart
+++ lib/presentation/profile_user_edit/views/user_level_view.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stepper/common/consts.dart';
@@ -10,12 +11,30 @@ import 'package:stepper/presentation/profile_user_edit/views/row_level_view.dart
 class UserLevelView extends StatelessWidget {
   const UserLevelView({Key? key}) : super(key: key);
 
-  _onProfessionChange(String professonName, BuildContext context) {
-    context.read<ProfileUserEditCubit>().onProfessionChanged(professonName);
+  _onProfessionChange(String professonName, BuildContext context) async {
+    final connectivityResult = await (Connectivity().checkConnectivity());
+
+    if (connectivityResult == ConnectivityResult.none) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(internetRequireToChangeProfession),
+        duration: Duration(milliseconds: 2000),
+      ));
+    } else {
+      context.read<ProfileUserEditCubit>().onProfessionChanged(professonName);
+    }
   }
 
-  _onBandChange(String bandName, BuildContext context) {
-    context.read<ProfileUserEditCubit>().onBandChanged(bandName);
+  _onBandChange(String bandName, BuildContext context) async {
+    final connectivityResult = await (Connectivity().checkConnectivity());
+
+    if (connectivityResult == ConnectivityResult.none) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text(internetRequireToChangeBand),
+        duration: Duration(milliseconds: 2000),
+      ));
+    } else {
+      context.read<ProfileUserEditCubit>().onBandChanged(bandName);
+    }
   }
 
   @override
