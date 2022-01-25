5fd3b99dfb3f5f4191f6ef127eed42e89152ccfb
Mai Thinh <maihophucthinh@gmail.com>
2022-01-25 10:30:00 +0700

    mthinh/restucture_home_screen * Create RecentUpdatedAreaCubit And State

--- lib/presentation/home/cubit/recent_updated_areas_state.dart
+++ lib/presentation/home/cubit/recent_updated_areas_state.dart
@@ -3,15 +3,14 @@ import 'package:stepper/data/model/area.dart';
 import 'package:stepper/enums/enums.dart';
 
 class RecentUpdatedAreasState extends Equatable {
-  final List<Area>? recentUpdatedAreas;
-  final CubitStateStatus? status;
+  final List<Area> recentUpdatedAreas;
+  final StateStatus? status;
 
   const RecentUpdatedAreasState(
-      {this.recentUpdatedAreas = const [],
-      this.status = CubitStateStatus.initial});
+      {this.recentUpdatedAreas = const [], this.status = StateStatus.initial});
 
   RecentUpdatedAreasState copyWith(
-          {List<Area>? recentUpdatedAreas, CubitStateStatus? status}) =>
+          {List<Area>? recentUpdatedAreas, StateStatus? status}) =>
       RecentUpdatedAreasState(
           recentUpdatedAreas: recentUpdatedAreas ?? this.recentUpdatedAreas,
           status: status ?? this.status);
