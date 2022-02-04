d8e43ad86fdef4c4908897e1aaf3cc96a06e4524
Mai Thinh <maihophucthinh@gmail.com>
2022-02-04 14:54:33 +0700

    rewrite profession firebase service

---
+++ lib/data/datasources/remote/firebase_services_2/profession_firebase_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:stepper/data/model2/model.dart';
+
+class ProfessionFirebaseService {
+  final FirebaseFirestore firestore;
+
+  ProfessionFirebaseService({required this.firestore});
+
+  Future<List<Profession>> getAllProfessions() async {
+    final querySnapshot = await firestore.collection('professions').get();
+    final professions = querySnapshot.docs.map((doc) {
+      return Profession.fromJson(doc.data());
+    }).toList();
+
+    return professions;
+  }
+
+  Future<Profession?> getProfessionById({required String professionId}) async {
+    try {
+      final querySnapshot =
+          await firestore.collection('professions').doc(professionId).get();
+      if (querySnapshot.exists) {
+        return Profession.fromJson(querySnapshot.data()!);
+      }
+      return null;
+    } catch (error) {}
+  }
+}
