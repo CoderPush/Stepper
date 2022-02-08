d2e2cd691dad228bd8bfbd3eb55850047521545d
Mai Thinh <maihophucthinh@gmail.com>
2022-02-08 09:47:51 +0700

    export post service/repo/repo_impl in index file

--- lib/data/datasources/remote/firebase_services_2/firebase_services.dart
+++ lib/data/datasources/remote/firebase_services_2/firebase_services.dart
@@ -3,3 +3,4 @@ export 'auth_firebase_service.dart';
 export 'band_firebase_service.dart';
 export 'profession_firebase_service.dart';
 export 'user_firebase_service.dart';
+export 'post_firebase_service.dart';
--- lib/domain/repositories2/repositories2.dart
+++ lib/domain/repositories2/repositories2.dart
@@ -3,3 +3,4 @@ export 'auth_repository.dart';
 export 'band_repository.dart';
 export 'profession_repository.dart';
 export 'user_repository.dart';
+export 'post_repository.dart';
