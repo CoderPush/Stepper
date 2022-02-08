885279f21f626294c2f0e8b870380fee60c2d7aa
Mai Thinh <maihophucthinh@gmail.com>
2022-02-08 09:45:13 +0700

    add method  subscribe_user_areas and get_user_areas for area firebase service

--- lib/data/datasources/remote/firebase_services_2/area_firebase_service.dart
+++ lib/data/datasources/remote/firebase_services_2/area_firebase_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:stepper/data/model2/models2.dart';
+import 'package:stepper/data/helpers/firestore_helpers.dart';
 
 class AreaFirebaseService2 {
   final FirebaseFirestore firestore;
@@ -40,5 +41,33 @@ class AreaFirebaseService2 {
     } catch (error) {
       return [];
     }
+  }
+
+  Future<List<Area>> getUserAreas() async {
+    final userDocSnap = await firestore.userDocument();
+    final areasSnap = await userDocSnap.areaCollection.get();
+    return areasSnap.docs
+        .map((doc) => Area.fromJson(doc.data() as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<List<Area>> getUserAreasByAreaTypeAndBandId(
+      {required String areaType, required String bandId}) async {
+    final userDocSnap = await firestore.userDocument();
+    final areasSnap = await userDocSnap.areaCollection
+        .where("type", isEqualTo: areaType)
+        .where("band_id", isEqualTo: bandId)
+        .get();
+    return areasSnap.docs
+        .map((doc) => Area.fromJson(doc.data() as Map<String, dynamic>))
+        .toList();
+  }
+
+  Stream<List<Area>> subscribeAreas() async* {
+    final userDocSnap = await firestore.userDocument();
+    final areaStream = userDocSnap.areaCollection.snapshots();
+    yield* areaStream.map((snapshot) => snapshot.docs
+        .map((doc) => Area.fromJson(doc.data() as Map<String, dynamic>))
+        .toList());
   }
 }
