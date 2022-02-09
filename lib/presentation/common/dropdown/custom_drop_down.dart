558d00065cfbfbd5e171054d3ac5e765310703ff
Mai Thinh <maihophucthinh@gmail.com>
2022-02-09 16:17:31 +0700

    add "disabled" | "disabledHintText" props for custom dropdown widget

--- lib/presentation/common/dropdown/custom_drop_down.dart
+++ lib/presentation/common/dropdown/custom_drop_down.dart
@@ -7,13 +7,17 @@ class CustomDropdown extends StatelessWidget {
   final Function(String) onChanged;
   final String value;
   final List<String> items;
+  final bool disabled;
+  final String disableHintText;
 
-  const CustomDropdown({
-    Key? key,
-    required this.onChanged,
-    required this.value,
-    required this.items,
-  }) : super(key: key);
+  const CustomDropdown(
+      {Key? key,
+      required this.onChanged,
+      required this.value,
+      required this.items,
+      this.disabled = false,
+      this.disableHintText = ""})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,25 +26,32 @@ class CustomDropdown extends StatelessWidget {
         buttonColor: white,
         alignedDropdown: true,
         child: DropdownButton2(
+          disabledHint: Text(
+            disableHintText,
+            style: const TextStyle(color: white),
+          ),
           dropdownDecoration: BoxDecoration(
             borderRadius: BorderRadius.circular(largeBorderRadius),
             color: dropdownButtonColor,
           ),
           iconEnabledColor: white,
+          iconDisabledColor: white,
           onChanged: (value) {
             onChanged(value as String);
           },
-          value: value,
-          items: items.map(
-            (item) {
-              return DropdownMenuItem(
-                child: FittedBox(
-                  child: Text(item),
-                ),
-                value: item,
-              );
-            },
-          ).toList(),
+          value: disabled ? null : value,
+          items: disabled
+              ? null
+              : items.map(
+                  (item) {
+                    return DropdownMenuItem(
+                      child: FittedBox(
+                        child: Text(item),
+                      ),
+                      value: item,
+                    );
+                  },
+                ).toList(),
         ),
       ),
     );
