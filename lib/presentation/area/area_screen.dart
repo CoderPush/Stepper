102bbf2f94775e3350a4666c1a73e046daf0b1a3
Mai Thinh <maihophucthinh@gmail.com>
2022-02-10 21:39:44 +0700

    Modify areas_screen to adapt new model and state

--- lib/presentation/area/area_screen.dart
+++ lib/presentation/area/area_screen.dart
@@ -3,9 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stepper/common/consts.dart';
 import 'package:stepper/common/palette.dart';
 import 'package:stepper/common/texts.dart';
-import 'package:stepper/data/model/models.dart';
+import 'package:stepper/data/model2/models2.dart';
+import 'package:stepper/enums/enums.dart';
 import 'package:stepper/injection_container.dart';
-import 'package:stepper/presentation/area/cubit/area_cubit.dart';
+import 'package:stepper/presentation/area/cubit/areas_cubit_2.dart';
+import 'package:stepper/presentation/area/cubit/areas_state_2.dart';
 import 'package:stepper/presentation/area/views/area_list.dart';
 import 'package:stepper/presentation/area/views/tabbar_item.dart';
 import 'package:stepper/presentation/common/drawer/drawer.dart';
@@ -17,10 +19,46 @@ class AreaScreen extends StatelessWidget {
     return const Center(child: CircularProgressIndicator());
   }
 
+  Widget buildAreasTab(AreasState state, {int? index}) {
+    if (AreaType.values.indexOf(state.selectedAreaType) != index) {
+      return _buildLoadingState();
+    }
+
+    if (state.fetchingStatus == StateStatus.loading) {
+      return _buildLoadingState();
+    }
+    if (state.fetchingStatus == StateStatus.failure) {
+      return const Text('Error loading scope area');
+    }
+
+    if (state.fetchingStatus == StateStatus.success) {
+      return AreaList(areaList: state.areas);
+    }
+
+    return _buildLoadingState();
+  }
+
+  onSelectAreaType(BuildContext context, {int? index}) {
+    switch (index) {
+      case 0:
+        context.read<AreasCubit>().onSelectAreaType(areaType: AreaType.scope);
+        break;
+      case 1:
+        context
+            .read<AreasCubit>()
+            .onSelectAreaType(areaType: AreaType.expertise);
+        break;
+      case 2:
+        context.read<AreasCubit>().onSelectAreaType(areaType: AreaType.mindset);
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AreaCubit(areaRepository: sl()),
+      create: (context) =>
+          AreasCubit(areaRepository: sl(), userRepository: sl()),
       child: Builder(builder: (context) {
         return DefaultTabController(
           length: 3,
@@ -29,97 +67,49 @@ class AreaScreen extends StatelessWidget {
               backgroundColor: darkPurple,
               title: const Text(area),
               centerTitle: true,
-              bottom: TabBar(
-                labelPadding: const EdgeInsets.all(0.0),
-                onTap: (index) {
-                  switch (index) {
-                    case 0:
-                      context.read<AreaCubit>().getAreas(AreaType.scope);
-                      break;
-                    case 1:
-                      context.read<AreaCubit>().getAreas(AreaType.expertise);
-                      break;
-                    case 2:
-                      context.read<AreaCubit>().getAreas(AreaType.mindset);
-                      break;
-                  }
-                },
-                indicatorColor: Colors.transparent,
-                tabs: [
-                  BlocBuilder<AreaCubit, AreaState>(
-                    builder: (context, state) {
-                      return Padding(
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(kToolbarHeight),
+                child: BlocBuilder<AreasCubit, AreasState>(
+                  builder: (context, state) => TabBar(
+                    labelPadding: const EdgeInsets.all(0.0),
+                    // TODO:
+                    onTap: (int index) =>
+                        onSelectAreaType(context, index: index),
+                    indicatorColor: Colors.transparent,
+                    tabs: [
+                      Padding(
                         padding:
                             const EdgeInsets.only(left: screenMediumPadding),
                         child: TabBarItem(
                           tabBarText: scope,
-                          isSelected: state.areaType == AreaType.scope,
+                          isSelected: state.selectedAreaType == AreaType.scope,
                         ),
-                      );
-                    },
-                  ),
-                  BlocBuilder<AreaCubit, AreaState>(
-                    builder: (context, state) {
-                      return TabBarItem(
+                      ),
+                      TabBarItem(
                         tabBarText: expertise,
-                        isSelected: state.areaType == AreaType.expertise,
-                      );
-                    },
-                  ),
-                  BlocBuilder<AreaCubit, AreaState>(
-                    builder: (context, state) {
-                      return Padding(
+                        isSelected:
+                            state.selectedAreaType == AreaType.expertise,
+                      ),
+                      Padding(
                         padding:
                             const EdgeInsets.only(right: screenMediumPadding),
                         child: TabBarItem(
                           tabBarText: mindset,
-                          isSelected: state.areaType == AreaType.mindset,
+                          isSelected:
+                              state.selectedAreaType == AreaType.mindset,
                         ),
-                      );
-                    },
+                      )
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
             body: SafeArea(
-              child: TabBarView(
-                physics: const NeverScrollableScrollPhysics(),
-                children: [
-                  BlocBuilder<AreaCubit, AreaState>(
-                    builder: (context, state) {
-                      if (state is AreaLoading) {
-                        return _buildLoadingState();
-                      } else if (state is AreaLoaded) {
-                        return AreaList(areaList: state.areaList);
-                      } else if (state is AreaError) {
-                        return const Text('Error loading scope area');
-                      }
-                      return _buildLoadingState();
-                    },
-                  ),
-                  BlocBuilder<AreaCubit, AreaState>(
-                    builder: (context, state) {
-                      if (state is AreaLoading) {
-                        return _buildLoadingState();
-                      } else if (state is AreaLoaded) {
-                        return AreaList(areaList: state.areaList);
-                      } else {
-                        return const Text('Error loading scope area');
-                      }
-                    },
-                  ),
-                  BlocBuilder<AreaCubit, AreaState>(
-                    builder: (context, state) {
-                      if (state is AreaLoading) {
-                        return _buildLoadingState();
-                      } else if (state is AreaLoaded) {
-                        return AreaList(areaList: state.areaList);
-                      } else {
-                        return const Text('Error loading scope area');
-                      }
-                    },
-                  ),
-                ],
+              child: BlocBuilder<AreasCubit, AreasState>(
+                builder: (context, state) => TabBarView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: List.generate(
+                        3, (index) => buildAreasTab(state, index: index))),
               ),
             ),
             drawer: MainDrawer(),
