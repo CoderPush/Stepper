2f024dca3ca334de4fd34ce3c85e34c4914a58f7
Mai Thinh <maihophucthinh@gmail.com>
2022-02-10 10:32:23 +0700

    use PageView for snap effect in horizontal_area_list

--- lib/presentation/home/views/horizontal_area_list.dart
+++ lib/presentation/home/views/horizontal_area_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stepper/common/consts.dart';
-import 'package:stepper/data/model/models.dart';
+import 'package:stepper/data/model2/models2.dart';
 import 'package:stepper/presentation/home/views/area_item.dart';
 
 class HorizontalAreaList extends StatelessWidget {
@@ -12,26 +12,27 @@ class HorizontalAreaList extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
 
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: List.generate(
-          areaList.length,
-          (index) => Padding(
-            padding: index == 0
-                ? const EdgeInsets.only(
-                    left: screenMediumPadding,
-                    right: screenMediumPadding,
-                  )
-                : const EdgeInsets.only(right: screenMediumPadding),
-            child: AreaItem(
-              areaSubTitle: sliderValues[areaList[index].rating],
-              itemWidth: screenSize.width * 0.6,
-              area: areaList[index],
-            ),
-          ),
-        ),
-      ),
+    return SizedBox(
+      height: 80,
+      child: PageView.builder(
+          padEnds: false,
+          controller: PageController(viewportFraction: 0.8),
+          itemCount: areaList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: index == 0
+                  ? const EdgeInsets.only(
+                      left: screenMediumPadding,
+                      right: screenMediumPadding,
+                    )
+                  : const EdgeInsets.only(right: screenMediumPadding),
+              child: AreaItem(
+                areaSubTitle: sliderValues[areaList[index].rating],
+                itemWidth: screenSize.width * 0.6,
+                area: areaList[index],
+              ),
+            );
+          }),
     );
   }
 }
