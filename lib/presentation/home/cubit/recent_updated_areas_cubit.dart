4b3fc029ab3d0e7572688c3dbdb1dcb74dc5c4b2
Mai Thinh <maihophucthinh@gmail.com>
2022-02-10 10:20:07 +0700

    Use subscribeAreas in recent_updated_areas in homescreen

--- lib/presentation/home/cubit/recent_updated_areas_cubit.dart
+++ lib/presentation/home/cubit/recent_updated_areas_cubit.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:stepper/domain/repositories/repositories.dart';
+import 'package:stepper/domain/repositories2/repositories2.dart';
 import 'package:stepper/enums/enums.dart';
 import 'package:stepper/presentation/home/cubit/recent_updated_areas_state.dart';
 
 class RecentUpdatedAreasCubit extends Cubit<RecentUpdatedAreasState> {
   AreaRepository areaRepository;
+  late StreamSubscription _areasSteamSubscription;
 
   RecentUpdatedAreasCubit({required this.areaRepository})
       : super(const RecentUpdatedAreasState()) {
@@ -14,11 +17,19 @@ class RecentUpdatedAreasCubit extends Cubit<RecentUpdatedAreasState> {
   Future<void> getRecentUpdatedAreas() async {
     try {
       emit(state.copyWith(status: StateStatus.loading));
-      final areas = await areaRepository.fetchRecentlyUpdatedAreas();
-      emit(state.copyWith(
-          status: StateStatus.success, recentUpdatedAreas: areas));
+      _areasSteamSubscription =
+          areaRepository.subscribeAreas().listen((areasList) {
+        emit(state.copyWith(
+            status: StateStatus.success, recentUpdatedAreas: areasList));
+      });
     } on Exception {
       emit(state.copyWith(status: StateStatus.failure));
     }
+  }
+
+  @override
+  Future<void> close() {
+    _areasSteamSubscription.cancel();
+    return super.close();
   }
 }
