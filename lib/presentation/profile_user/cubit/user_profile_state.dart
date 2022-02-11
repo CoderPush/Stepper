489e9f693d5ef35e6ea3796c64a59fa5d04786c6
Mai Thinh <maihophucthinh@gmail.com>
2022-02-11 13:41:41 +0700

    Define new state for user profile screen

---
+++ lib/presentation/profile_user/cubit/user_profile_state.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+import 'package:stepper/data/model2/models2.dart';
+import 'package:stepper/enums/enums.dart';
+
+class UserProfileState extends Equatable {
+  final User user;
+  final StateStatus fetchingStatus;
+
+  UserProfileState({User? user, this.fetchingStatus = StateStatus.initial})
+      : user = user ?? User.empty;
+
+  UserProfileState copyWith({User? user, StateStatus? fetchingStatus}) =>
+      UserProfileState(
+          user: user ?? this.user,
+          fetchingStatus: fetchingStatus ?? this.fetchingStatus);
+
+  @override
+  List<Object?> get props => [user, fetchingStatus];
+}
