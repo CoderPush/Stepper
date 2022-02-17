4e596c3bcf738d506d155d5e3c0f6c24ccd16531
Mai Thinh <maihophucthinh@gmail.com>
2022-02-17 15:01:23 +0700

    remove unused import in utils

--- lib/presentation/utils.dart
+++ lib/presentation/utils.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:stepper/common/palette.dart';
 import 'package:stepper/data/models/models.dart';
