7c61d471f85c3a102d6720fffd2fbe5d7a2efd82
Mai Thinh <maihophucthinh@gmail.com>
2022-02-17 10:47:16 +0700

    * remove unused dependency

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,56 +2,60 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_it/get_it.dart';
-import 'package:stepper/data/datasources/remote/services.dart';
+import 'package:stepper/data/datasources/firebase_services/firebase_services.dart';
+import 'package:stepper/data/helpers/precache_firestore_handler.dart';
+import 'package:stepper/data/repositories_impl/repositories_impl.dart';
 import 'package:stepper/domain/repositories/repositories.dart';
-import 'package:stepper/data/repositories/repositories_impl.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
-  // Features
-  sl.registerLazySingleton<AreaRepository>(() => AreaRepositoryImpl(
-        bandService: sl(),
-        settingFirebaseService: sl(),
-        postFirebaseService: sl(),
-        areaFirebaseService: sl(),
-      ));
+  sl.registerLazySingleton<ProfessionFirebaseService>(
+      () => ProfessionFirebaseService(firestore: sl()));
 
-  sl.registerLazySingleton<ProfessionRepository>(() => ProfessionRepositoryImpl(
-        professionService: sl(),
-        settingFirebaseService: sl(),
-      ));
-
-  sl.registerLazySingleton<BandRepository>(() =>
-      BandRepositoryImpl(bandService: sl(), settingFirebaseService: sl()));
-
-  sl.registerLazySingleton<GoalRepository>(
-      () => GoalRepositoryImpl(goalDatabase: sl()));
-
-  sl.registerLazySingleton<PostRepository>(
-      () => PostRepositoryImpl(postFirebaseService: sl()));
-
-  sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(
-        firebaseAuth: sl(),
-        areaFirebaseService: sl(),
-        areaService: sl(),
-        settingFirebaseService: sl(),
-      ));
-
-  sl.registerLazySingleton<AreaService>(() => AreaService());
-
-  sl.registerLazySingleton<ProfessionService>(() => ProfessionService());
-
-  sl.registerLazySingleton<BandService>(() => BandService());
-
-  sl.registerLazySingleton<PostFirebaseService>(
-      () => PostFirebaseService(firestore: sl(), firebaseStorage: sl()));
+  sl.registerLazySingleton<BandFirebaseService>(
+      () => BandFirebaseService(firestore: sl()));
 
   sl.registerLazySingleton<AreaFirebaseService>(
       () => AreaFirebaseService(firestore: sl()));
 
-  sl.registerLazySingleton<SettingFirebaseService>(
-      () => SettingFirebaseService(firestore: sl()));
+  sl.registerLazySingleton<AuthFirebaseService>(
+      () => AuthFirebaseService(firebaseAuth: sl()));
+
+  sl.registerLazySingleton<UserFirebaseService>(
+      () => UserFirebaseService(firestore: sl(), firebaseStorage: sl()));
+
+  sl.registerLazySingleton<PostFirebaseService>(
+      () => PostFirebaseService(firestore: sl()));
+
+  sl.registerLazySingleton<ProfessionRepository>(
+      () => ProfessionRepositoryImpl(professionFirebaseService: sl()));
+
+  sl.registerLazySingleton<BandRepository>(
+      () => BandRepositoryImpl(bandFirebaseService: sl()));
+
+  sl.registerLazySingleton<AreaRepository>(
+      () => AreaRepositoryImpl(areaFirebaseService: sl()));
+
+  sl.registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(authFirebaseService: sl()));
+
+  sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(
+      userFirebaseService: sl(),
+      authRepository: sl(),
+      professionRepository: sl(),
+      bandRepository: sl()));
+
+  sl.registerLazySingleton<PostRepository>(
+      () => PostRepositoryImpl(postFirebaseService: sl()));
+
+  sl.registerLazySingleton<PreCacheFirestoreHandler>(() =>
+      PreCacheFirestoreHandler(
+          authFirebaseService: sl(),
+          userFirebaseService: sl(),
+          professionFirebaseService: sl(),
+          bandFirebaseService: sl(),
+          areaFirebaseService: sl()));
 
   sl.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
 
