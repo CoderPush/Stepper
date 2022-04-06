784df8e7283e57ee9ec8238f6dfd1266760a4dd4
Mai Thinh <maihophucthinh@gmail.com>
2022-04-06 15:25:36 +0700

    Sign in with Google (#188)
    * add signInWithCredential and signInWithPopup
    
    * Change UI login screen
    
    * Change title of web app
    
    * delete unused authcard widget
    
    * handle auth flow using google and firebase
    
    * add google logo and stepper logo in assets
    
    * Modify return type of getUser
    
    * automate adding reversed client id in info plist for google sign in
    
    * edit path to info.plist
    
    * fix not break line when define env var
    
    * handle logut with new route defined

--- lib/presentation/authentication/cubit/authentication_cubit.dart
+++ lib/presentation/authentication/cubit/authentication_cubit.dart
@@ -1,14 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:stepper/config/extensions/string_extensions.dart';
 import 'package:stepper/domain/exceptions/exceptions.dart';
 import 'package:stepper/domain/repositories/repositories.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 part 'authentication_state.dart';
 
 class AuthenticationCubit extends Cubit<AuthenticationState> {
   final AuthRepository authRepository;
   final UserRepository userRepository;
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: ["email", "https://www.googleapis.com/auth/userinfo.profile"]);
 
   AuthenticationCubit({
     required this.authRepository,
@@ -34,45 +39,77 @@ class AuthenticationCubit extends Cubit<AuthenticationState> {
     }
   }
 
+  Future<UserCredential> signInGoogleWeb() async {
+    GoogleAuthProvider authProvider = GoogleAuthProvider();
+    authProvider.setCustomParameters({"prompt": "select_account"});
+    final userCredential = await authRepository.signInWithPopup(authProvider);
+    return userCredential;
+  }
+
+  Future<UserCredential?> signInGoogleMobile() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    if (googleUser == null) return null;
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    final userCredential =
+        await authRepository.signInWithCredential(credential);
+    return userCredential;
+  }
+
   bool isUserSignedIn() => authRepository.authUser != null;
 
-  Future<void> signInWithEmailAndPassword(
-      String emailAddress, String password) async {
+  Future<void> signIn() async {
+    late UserCredential? userCredential;
     try {
-      await authRepository.signIn(email: emailAddress, password: password);
+      if (kIsWeb) {
+        userCredential = await signInGoogleWeb();
+      } else {
+        userCredential = await signInGoogleMobile();
+      }
+
+      if (userCredential == null) return;
+
+      final isUserProfileCreated = await _checkUserProfileCreated();
+      if (!isUserProfileCreated) {
+        await _createInitialUserProfile();
+      }
+
+      final email = userCredential.user!.email!;
       emit(AuthenticatedState(
-        userEmail: emailAddress,
-        userName: emailAddress.split('@')[0].capitalizeFirstLetter(),
+        userEmail: email,
+        userName: email.split('@')[0].capitalizeFirstLetter(),
       ));
-    } on AuthException catch (e) {
+    } catch (e) {
       emit(AuthenticationError(e.toString()));
     }
   }
 
-  Future<void> registerWithEmailAndPassword(
-      String emailAddress, String password) async {
-    try {
-      await authRepository.signUp(email: emailAddress, password: password);
-      await _createInitialUserData();
-      emit(AuthenticatedState(
-        userEmail: emailAddress,
-        userName: emailAddress.split('@')[0].capitalizeFirstLetter(),
-      ));
-    } on AuthException catch (e) {
-      emit(AuthenticationError(e.toString()));
-    }
-  }
-
-  _createInitialUserData() async {
+  _createInitialUserProfile() async {
     await userRepository.createUser();
+  }
+
+  Future<bool> _checkUserProfileCreated() async {
+    final userProfile = await userRepository.getUser();
+    return userProfile != null;
   }
 
   Future<void> onUserSignOut() async {
     try {
       emit(AuthenticationInitial());
-      await Future.delayed(const Duration(seconds: 1));
+      if (!kIsWeb) {
+        await _googleSignIn.signOut();
+      }
+
       await authRepository.signOut();
-      emit(UnauthenticatedState());
     } on AuthException catch (e) {
       emit(AuthenticationError(e.toString()));
     }
