d277fda3c7061f351fcafefd1dc11d30fea9e440
conghua <conghua2411@gmail.com>
2022-05-13 10:04:28 +0700

    fix web auth screen crash

--- lib/presentation/authentication/authentication_screen.dart
+++ lib/presentation/authentication/authentication_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
@@ -54,7 +55,7 @@ class AuthenticationScreen extends StatelessWidget {
                 SizedBox(
                   height: 12.sp,
                 ),
-                Platform.isIOS
+                !kIsWeb && Platform.isIOS
                     ? LoginButton(
                         icon: SvgPicture.asset(
                           "assets/svg/apple_logo.svg",
